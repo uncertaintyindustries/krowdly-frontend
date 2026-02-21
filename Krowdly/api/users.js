4fbe38946bc08924ca21973357c40ec4cc8cc92d
uncertaintyindustries <kristonitechris2010@gmail.com>
2026-02-21 19:27:57 +0100

    Add files via upload

---
+++ Krowdly/api/users.js
@@ -0,0 +1,5 @@
+let users = [{ id: "demo", username: "Demo User", photoURL: "https://picsum.photos/50", following: [] }];
+
+export default function handler(req, res) {
+  res.status(200).json(users);
+}
